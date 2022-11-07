529d5544c3961cd25d2fa731117d4a5e4d4b96bd
Aydan Abrahams <me@aydan.co.uk>
2022-11-07 19:31:10 +0000

    Push

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "eslint-config-es5",
+    "plugins": [
+        "import"
+    ],
+    "rules": {
+        "strict": [0, "global"],
+        "no-var": "off"
+    }
+};
